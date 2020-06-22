0c5304023a2d385e676244c72f75af256dc4afc3
Leroy Grant <lgrant146@gmail.com>
2020-06-22 14:53:55 -0500

    almost done

--- src/FileIoMiniExercise.java
+++ src/FileIoMiniExercise.java
@@ -5,30 +5,46 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FileIoMiniExercise {
+public class FileIoMiniExercise extends Instructor {
+    public FileIoMiniExercise(String name) {
+        super(name);
+    }
+
     public static void main(String[] args) {
 
         // TODO: define the path to the instructor-names.txt file
+            Path FileInstructors = Paths.get("src", "instructor-names.txt");
+
 
 
         // TODO: uncomment the following variable declarations
-//        List<Instructor> instructors;
-//        List<String> names = new ArrayList<>();
+        List<Instructor> instructors;
+        List<String> names = new ArrayList<>();
 
         // TODO: read the contents of the instructor-names.txt file and store the list of strings into the 'names' variable
-
+        try {
+            names = Files.readAllLines(FileInstructors);
+            System.out.println(names);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         // TODO: assign the 'instructors' variable a list of Instructor objects with names matching the list of strings/names from the text file (the Instructor class has a helpful method for this)
-
+            instructors = nameStringsToInstructors(names);
+        System.out.println(instructors);
 
         // TODO: greet all instructors by their names
 
+            greetInstructors(instructors);
+
 
         // TODO: change "Fred" to "David" in the list of Instructor objects
-
+        System.out.println("==========================");
+            changeFredToDavid(instructors);
+            greetInstructors(instructors);
 
         // TODO: update the list of strings/names ('names' variable) to the latest names from the Instructor objects (the Instructor class has a helpful method for this)
-
+//            instructors = nameStringsToInstructors(instructors);
 
         // TODO: overwrite the instructors-names.txt file with the latest list of names
 
